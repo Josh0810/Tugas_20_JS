cea43feed9a6f19e7269f8d2d27f46661c8220da
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2021-12-29 03:05:45 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,5 @@
+function Tugas20() {
+    var p = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+    console.log(p.match(/are/), p.match("words"));
+}
+Tugas20()
